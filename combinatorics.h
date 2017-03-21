20f4b27edb22f64c53d154c2db879627c4ce49c1
cjamesm <christopherjmurphy1990@gmail.com>
2017-03-21 22:46:55 +0000

    :Just a little bit:

---
+++ combinatorics.h
@@ -0,0 +1,6 @@
+#pragma once
+
+
+int permutations(int n, int k);
+int combinations(int n, int k);
+int fact(int n);
---
+++ stanfordChaper3.cpp
@@ -0,0 +1,11 @@
+//stanfordChapter3.cpp: naughtily skipping over some chap.2 exercises to get ahead
+
+#include "stdafx.h"
+#include <iostream>
+
+
+//Chapter 3 has 20 exercises. Good luck with that.
+int main()
+{
+	return 0;
+}
